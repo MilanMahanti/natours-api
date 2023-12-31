9d9e243f6825bf190f5e42f714b7a5535bb49e6e
Milan <milanmahanti16@gmail.com>
2023-12-31 10:44:06 +0530

    changed validate in express-rate-limiter

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
-  xForwardedForHeader: false,
+  validate: { xForwardedForHeader: false },
 });
 app.use('/api', limiter);
 
